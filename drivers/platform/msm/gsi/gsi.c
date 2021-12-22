49899d14a2cca59d74ade63ff49d74449c698dad
Ashok Vuyyuru <quic_avuyyuru@quicinc.com>
2021-12-23 00:06:59 +0530

    msm: ipa3: Deepsleep enable changes
    Adding support to deepsleep scenario.
    
    Change-Id: I2b388396cf9a7f26f77bbcd49c1014e16475a980

--- drivers/platform/msm/gsi/gsi.c
+++ drivers/platform/msm/gsi/gsi.c
@@ -1839,8 +1839,7 @@ int gsi_deregister_device(unsigned long dev_hdl, bool force)
 	devm_free_irq(gsi_ctx->dev, gsi_ctx->per.irq, gsi_ctx);
 	gsihal_destroy();
 	gsi_unmap_base();
-	memset(gsi_ctx, 0, sizeof(*gsi_ctx));
-
+	gsi_ctx->per_registered = false;
 	return GSI_STATUS_SUCCESS;
 }
 EXPORT_SYMBOL(gsi_deregister_device);
