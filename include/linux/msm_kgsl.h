ac2b41f82777d5942df75e8e2acd1c2060d502ad
Hareesh Gundu <quic_hareeshg@quicinc.com>
2022-01-06 08:58:35 +0530

    msm: kgsl: Add support for SRCU notifiers
    EVA kernel driver need to handle gpu fault and
    GMU readiness notification. Hence add support
    for SRCU notifier to deal with these events.
    
    Change-Id: I5426d64ae0e6bc64e56aabbf6fb4aa00cd8cb898
    Signed-off-by: Hareesh Gundu <quic_hareeshg@quicinc.com>

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #ifndef _MSM_KGSL_H
 #define _MSM_KGSL_H
@@ -47,6 +48,38 @@ int kgsl_gpu_stat(struct kgsl_gpu_freq_stat *stats, u32 numfreq);
  * to @pid in @frame_count or negative error on failure
  */
 int kgsl_gpu_frame_count(pid_t pid, u64 *frame_count);
+
+/**
+ * enum kgsl_srcu_events - kgsl srcu notify events for listeners
+ * @GPU_GMU_READY - GMU initialized to handle other requests
+ * @GPU_GMU_STOP - GMU is not ready to handle other requests
+ * @GPU_SSR_BEGIN - GPU/GMU fault detected to start recovery
+ * @GPU_SSR_END - GPU/GMU fault recovery end
+ * @GPU_SSR_FATAL - LSR context invalidated
+ */
+enum kgsl_srcu_events {
+	GPU_GMU_READY,
+	GPU_GMU_STOP,
+	GPU_SSR_BEGIN,
+	GPU_SSR_END,
+	GPU_SSR_FATAL,
+};
+
+/**
+ * kgsl_add_rcu_notifier - Adds a notifier to an SRCU notifier chain.
+ * @nb: Notifier block new entry to add in notifier chain
+ *
+ * Returns zero on success or error on failure.
+ */
+int kgsl_add_rcu_notifier(struct notifier_block *nb);
+
+/**
+ * kgsl_del_rcu_notifier - Remove notifier from an SRCU notifier chain.
+ * @nb: Entry to remove from notifier chain
+ *
+ * Returns zero on success or -ENOENT on failure.
+ */
+int kgsl_del_rcu_notifier(struct notifier_block *nb);
 
 #endif /* _MSM_KGSL_H */
 
