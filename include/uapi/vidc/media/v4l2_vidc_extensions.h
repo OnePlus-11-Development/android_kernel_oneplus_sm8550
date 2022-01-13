eqs-development/android_kernel_motorola_sm8475-modules

video: driver: add support for timestamp reorder
Added support for timestamp reorder. It is default
disabled.

Change-Id: I51a6fc785860081b651fc67f443132cb2675b9fd
Signed-off-by: Govindaraj Rajagopal <quic_grajagop@quicinc.com>

qcom/opensource/video-driver/include/uapi/vidc/media/v4l2_vidc_extensions.h
@@ -139,6 +139,8 @@ enum v4l2_mpeg_vidc_blur_types {
 /* Decoder Max Number of Reorder Frames */
 #define V4L2_CID_MPEG_VIDC_METADATA_MAX_NUM_REORDER_FRAMES                   \
 	(V4L2_CID_MPEG_VIDC_BASE + 0x30)
+/* Decoder Timestamp Reorder control */
+#define V4L2_CID_MPEG_VIDC_TS_REORDER           (V4L2_CID_MPEG_VIDC_BASE + 0x31)
 
 /* Deprecate below controls once availble in gki and gsi bionic header */
 #ifndef V4L2_CID_MPEG_VIDEO_BASELAYER_PRIORITY_ID

OnePlus-11-Development/android_kernel_oneplus_sm8550

include/uapi/vidc/media/v4l2_vidc_extensions.h
@@ -189,6 +189,8 @@ enum v4l2_mpeg_video_av1_tier {
 	V4L2_MPEG_VIDEO_AV1_TIER_MAIN  = 0,
 	V4L2_MPEG_VIDEO_AV1_TIER_HIGH  = 1,
 };
+/* Decoder Timestamp Reorder control */
+#define V4L2_CID_MPEG_VIDC_TS_REORDER           (V4L2_CID_MPEG_VIDC_BASE + 0x34)
 
 /* Deprecate below controls once availble in gki and gsi bionic header */
 #ifndef V4L2_CID_MPEG_VIDEO_BASELAYER_PRIORITY_ID