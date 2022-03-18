e763f5f7c0ce6f2477b023b19307e7db2c244c11
Mark Bao <quic_mbao@quicinc.com>
2022-03-18 14:56:27 -0700

    msm-mmrm: fixed para virtualization backend
    fixed para virtualization backend.
    
    Change-Id: Id7580187c444f5387ed7aa38e8ed441a02d4b2c6

--- vm/be/src/mmrm_vm_be_dispatch.c
+++ vm/be/src/mmrm_vm_be_dispatch.c
@@ -2,7 +2,6 @@
 /*
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
-
 #include "mmrm_vm_debug.h"
 #include "mmrm_vm_interface.h"
 #include "mmrm_vm_msgq.h"
@@ -62,8 +61,8 @@ static int mmrm_vm_be_client_register(struct mmrm_vm_driver_data *mmrm_vm,
 		mmrm_vm->clk_client_tbl[pClient->client_uid] = pClient;
 		pkt.msg.data.reg.client_id = pClient->client_uid;
 	} else {
-		pkt.msg.data.reg.client_id = 0;
-		d_mpr_e("%s: client:%p\n", __func__, pClient);
+		pkt.msg.data.reg.client_id = U32_MAX;
+		d_mpr_e("%s: client:%p client id:%d\n", __func__, pClient, pkt.msg.data.reg.client_id);
 	}
 
 	// prepare response packet & send to fe on SVM
@@ -96,6 +95,9 @@ static int mmrm_vm_be_client_setvalue(struct mmrm_vm_driver_data *mmrm_vm,
 	rc = mmrm_client_set_value(mmrm_vm->clk_client_tbl[req_param->client_id],
 			&req_param->data, req_param->val);
 
+	if (rc != 0) {
+		d_mpr_e("%s: set value rc:%d client id:%d\n", __func__, rc, req_param->client_id);
+	}
 	// prepare response packet & send to fe on SVM
 	pkt_resp.msg.hd.cmd_id = MMRM_VM_RESPONSE_SETVALUE;
 	pkt_resp.msg.hd.seq_no = req->hd.seq_no;
@@ -242,3 +244,24 @@ int mmrm_vm_be_recv(struct mmrm_vm_driver_data *mmrm_vm, void *data, size_t size
 	return rc;
 }
 
+/**
+ * mmrm_vm_be_wrong_request - deal with SVM wrong request
+ * mmrm_vm: driver private data
+ */
+int mmrm_vm_be_wrong_request(struct mmrm_vm_driver_data *mmrm_vm)
+{
+	int rc = 0;
+	struct mmrm_vm_response_msg_pkt pkt;
+
+	pkt.msg.hd.cmd_id = MMRM_VM_RESPONSE_INVALID_PKT;
+	pkt.msg.hd.seq_no = 0;
+
+	pkt.hdr.size = sizeof(pkt.msg.hd) + sizeof(pkt.msg.data.dereg);
+	pkt.msg.data.dereg.ret_code = rc;
+
+	d_mpr_e("%s: wrong request\n", __func__);
+	rc = mmrm_vm_be_send_response(mmrm_vm, &pkt);
+	if (rc != 0)
+		d_mpr_e("%s: rc:%d\n", __func__, rc);
+	return rc;
+}
--- vm/common/inc/mmrm_vm_interface.h
+++ vm/common/inc/mmrm_vm_interface.h
@@ -27,8 +27,9 @@ struct mmrm_vm_thread_info {
 	struct list_head   queued_msg;
 };
 
-/*
+/**
  * struct mmrm_vm_data_priv -- device driver private part
+ * @dev: device pointer
  * @msg_info: gunyah message info
  * @thread_info: message lister & workqueue info
  * @clk_client_tbl: index and client handler LUT
@@ -36,6 +37,7 @@ struct mmrm_vm_thread_info {
  * @vm_pvt_data: pointer to fe/be specific data
  */
 struct mmrm_vm_driver_data {
+	struct device *dev;
 	struct mmrm_vm_gh_msgq_info msg_info;
 	struct mmrm_vm_thread_info thread_info;
 	struct mmrm_client **clk_client_tbl;
@@ -45,7 +47,7 @@ struct mmrm_vm_driver_data {
 	void *vm_pvt_data;
 };
 
-/*
+/**
  * enum mmrm_vm_api_msg_id -- request/response cmd ID
  */
 enum mmrm_vm_api_msg_id {
@@ -60,9 +62,10 @@ enum mmrm_vm_api_msg_id {
 	MMRM_VM_RESPONSE_SETVALUE_INRANGE,
 	MMRM_VM_RESPONSE_GETVALUE,
 	MMRM_VM_RESPONSE_DEREGISTER,
+	MMRM_VM_RESPONSE_INVALID_PKT,
 };
 
-/*
+/**
  * struct msg_head -- message head
  * @cmd_id: mmrm API message cmd id
  * @seq_no: message sequence id
@@ -72,7 +75,7 @@ struct mmrm_vm_api_msg_head {
 	int  seq_no;
 };
 
-/*
+/**
  * struct register_request -- mmrm register parameters
  * @client_type: client type, definition see msm_mmrm.h
  * @priority: client priority, definition see msm_mmrm.h
@@ -84,7 +87,7 @@ struct mmrm_vm_register_request {
 	struct mmrm_clk_client_desc desc;
 };
 
-/*
+/**
  * struct deregister_request -- mmrm deregister parameters
  * @client: client registered handle
  */
@@ -92,7 +95,7 @@ struct mmrm_vm_deregister_request {
 	u32 client_id;
 };
 
-/*
+/**
  * struct setvalue_request -- mmrm setvalue parameters
  * @client: client type, definition see msm_mmrm.h
  * @data: client info, definition see msm_mmrm.h
@@ -104,7 +107,7 @@ struct mmrm_vm_setvalue_request {
 	unsigned long val;
 };
 
-/*
+/**
  * struct mmrm_vm_setvalue_inrange_request -- mmrm setvalue_inrange parameters
  * @client: client type, definition see msm_mmrm.h
  * @data: client info, definition see msm_mmrm.h
@@ -116,7 +119,7 @@ struct mmrm_vm_setvalue_inrange_request {
 	struct mmrm_client_res_value val;
 };
 
-/*
+/**
  * struct mmrm_vm_getvalue_request -- mmrm getvalue parameters
  * @client: client type, definition see msm_mmrm.h
  * @val: current clock rate value range, definition see msm_mmrm.h
@@ -125,7 +128,7 @@ struct mmrm_vm_getvalue_request {
 	u32 client_id;
 };
 
-/*
+/**
  * struct mmrm_vm_api_request_msg -- mmrm request API message unified data definition
  * @hd: mmrm API request message head
  * @data: parameters mmrm API needs per API message cmd id
@@ -141,7 +144,7 @@ struct mmrm_vm_api_request_msg {
 	} data;
 };
 
-/*
+/**
  * struct mmrm_vm_register_response -- mmrm_client_register API response message
  * @client: handle for registered client
  */
@@ -149,7 +152,7 @@ struct mmrm_vm_register_response {
 	u32 client_id;
 };
 
-/*
+/**
  * struct mmrm_vm_deregister_response -- mmrm_client_deregister API response message
  * @ret_code: indicates if the mmrm_client_deregister is successful
  */
@@ -157,7 +160,7 @@ struct mmrm_vm_deregister_response {
 	int ret_code;
 };
 
-/*
+/**
  * struct mmrm_vm_setvalue_response -- mmrm_client_set_value API response message
  * @val: value that mmrm_client_set_value return
  */
@@ -165,7 +168,7 @@ struct mmrm_vm_setvalue_response {
 	unsigned long val;
 };
 
-/*
+/**
  * struct mmrm_vm_setvalue_inrange_response -- mmrm_client_set_value_in_range API response message
  * @ret_code: value that mmrm_client_set_value_in_range return
  */
@@ -173,7 +176,7 @@ struct mmrm_vm_setvalue_inrange_response {
 	int ret_code;
 };
 
-/*
+/**
  * struct mmrm_vm_getvalue_response -- mmrm_client_get_value API response message
  * @val: value that mmrm_client_get_value return
  */
@@ -181,7 +184,7 @@ struct mmrm_vm_getvalue_response {
 	struct mmrm_client_res_value val;
 };
 
-/*
+/**
  * struct mmrm_vm_api_response_msg -- mmrm response message unified data
  * @hd: mmrm API response message head
  * @data: data that mmrm API return per API response message id
@@ -197,7 +200,7 @@ struct mmrm_vm_api_response_msg {
 	} data;
 };
 
-/*
+/**
  * struct mmrm_vm_request_msg_pkt -- mmrm request packet that is sent through gunyah API
  * @hdr: message head for checking message valid
  * @msg: data that is needed by mmrm API
@@ -207,7 +210,7 @@ struct mmrm_vm_request_msg_pkt {
 	struct mmrm_vm_api_request_msg msg;
 };
 
-/*
+/**
  * struct mmrm_vm_response_msg_pkt -- mmrm response packet that is sent through gunyah API
  * @hdr: message head for checking message valid
  * @msg: data that is returned by mmrm API
