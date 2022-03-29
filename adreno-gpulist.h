0efdb871175f45443e8900a34713bfaaa333c6c9
Mohammed Mirza Mandayappurath Manzoor <quic_mmandaya@quicinc.com>
2022-03-29 00:16:16 -0700

    msm: kgsl: Enable Adaptive Clock Distribution on Gen7_2_0
    ACD detects voltage droops and throttles the clocks.
    
    Change-Id: I2dceb06e6d5d028f6982c21cba796a3a58ca21f9
    Signed-off-by: Mohammed Mirza Mandayappurath Manzoor <quic_mmandaya@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -2037,7 +2037,7 @@ static const struct adreno_gen7_core adreno_gpu_core_gen7_2_0 = {
 		.compatible = "qcom,adreno-gpu-gen7-2-0",
 		.chipid = 0x43050a00,
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT | ADRENO_IFPC |
-				ADRENO_CONTENT_PROTECTION,
+				ADRENO_CONTENT_PROTECTION | ADRENO_ACD,
 		.gpudev = &adreno_gen7_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen7_2_0_perfcounters,
 		.gmem_base = 0x1000000,
