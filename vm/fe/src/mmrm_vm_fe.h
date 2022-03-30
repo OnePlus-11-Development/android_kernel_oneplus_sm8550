23b9e30ab65b6dfd1e59086b17f724d5ea7eaf26
Mark Bao <quic_mbao@quicinc.com>
2022-03-30 08:28:34 -0700

    msm-mmrm: fix para virtualization FrontEnd driver
    Fix para virtualization FrontEnd driver.
    
    Change-Id: Iaf2c6ea2410aff298d33641edfa2fa36376392f8

--- vm/fe/src/mmrm_vm_fe.h
+++ vm/fe/src/mmrm_vm_fe.h
@@ -9,14 +9,16 @@
 #include <mmrm_vm_msgq.h>
 #include <mmrm_vm_interface.h>
 
-struct mmrm_vm_fe_clk_src_info {
-	struct mmrm_clk_client_desc client_desc;
-	int flags;
+#define MMRM_SYSFS_ENTRY_MAX_LEN     PAGE_SIZE
+
+struct mmrm_vm_fe_clk_client_desc {
+	u32 client_domain;
+	u32 client_id;
+	u32 num_hw_blocks;
 };
 
-
 struct mmrm_vm_fe_clk_src_set {
-	struct mmrm_vm_fe_clk_src_info *clk_src_tbl;
+	struct mmrm_vm_fe_clk_client_desc *clk_src_tbl;
 	u32 count;
 };
 
@@ -25,12 +27,19 @@ struct mmrm_vm_fe_priv {
 
 	struct mmrm_client *client_tbl;
 
-	struct list_head mmrm_work_list;
-	struct mutex work_list_lock;
+	struct list_head resp_works;
+	struct mutex resp_works_lock;
 
 	struct mmrm_vm_fe_clk_src_set clk_src_set;
 	struct mutex msg_send_lock;
 	int  seq_no;
+	bool is_clk_scaling_supported;
+};
+
+struct mmrm_vm_fe_pkt {
+	struct mmrm_vm_msg_q msgq;
+	struct mmrm_vm_request_msg_pkt req_pkt;
+	struct mmrm_vm_response_msg_pkt resp_pkt;
 };
 
 /*
@@ -64,12 +73,40 @@ struct mmrm_client *mmrm_vm_fe_get_client(u32 client_id);
 int mmrm_vm_fe_load_clk_rsrc(struct mmrm_vm_driver_data *drv_priv);
 
 /*
+ * mmrm_vm_fe_clk_src_check -- check if fe support the clk src
+ * @desc: clk src description
+ */
+struct mmrm_vm_fe_clk_client_desc *mmrm_vm_fe_clk_src_get(struct mmrm_client_desc *desc);
+
+/*
+ * init_lookup_table -- init et clk lookup table
+ * @mmrm_vm: device data
+ */
+int mmrm_vm_fe_init_lookup_table(struct mmrm_vm_driver_data *mmrm_vm);
+
+/*
+ * mmrm_vm_fe_clk_print_info -- output clk info through sys
+ * @clk_src_set: clk info
+ * @buf: received output buffer
+ * @max_len: buffer length
+ */
+int mmrm_vm_fe_clk_print_info(
+	struct mmrm_vm_fe_clk_src_set *clk_src_set,
+	char *buf, int max_len);
+
+/*
  * mmrm_vm_fe_recv -- process received response info
  * @mmrm_vm: device data
  * @data: received response info buffer
  * @size: message size
  */
 void mmrm_vm_fe_recv(struct mmrm_vm_driver_data *mmrm_vm, void *data, size_t size);
+
+/*
+ * mmrm_vm_fe_count_clk_clients_frm_dt -- process received response info
+ * @pdev: platform device
+ */
+int mmrm_vm_fe_count_clk_clients_frm_dt(struct platform_device *pdev);
 
 #endif /* __MMRM_VM_FE_H__ */
 
--- vm/fe/src/mmrm_vm_fe_frontend.c
+++ vm/fe/src/mmrm_vm_fe_frontend.c
@@ -4,6 +4,7 @@
  */
 
 #include <linux/of.h>
+#include <linux/limits.h>
 
 #include "mmrm_vm_fe.h"
 #include "mmrm_vm_interface.h"
@@ -20,24 +21,26 @@ void mmrm_vm_fe_recv(struct mmrm_vm_driver_data *mmrm_vm, void *data, size_t siz
 	u64 kt2;
 	struct mmrm_vm_fe_priv *fe_data = mmrm_vm->vm_pvt_data;
 
-	mutex_lock(&fe_data->work_list_lock);
-	list_for_each_entry_safe(node, temp, &fe_data->mmrm_work_list, link) {
+	mutex_lock(&fe_data->resp_works_lock);
+	list_for_each_entry_safe(node, temp, &fe_data->resp_works, link) {
 		if (msg->hd.seq_no == node->m_req->msg.hd.seq_no) {
-			d_mpr_e("%s: seq no:%d\n", __func__, msg->hd.seq_no);
+			d_mpr_w("%s: seq no:%d\n", __func__, msg->hd.seq_no);
 			list_del(&node->link);
 			rc = 0;
 			break;
 		}
 	}
-	mutex_unlock(&fe_data->work_list_lock);
-	if (rc != 0)
+	mutex_unlock(&fe_data->resp_works_lock);
+	if (rc != 0) {
+		d_mpr_e("%s: seq no:%d wrong\n", __func__, msg->hd.seq_no);
 		return;
+	}
 
-	d_mpr_e("%s: cmd:%d\n", __func__, msg->hd.cmd_id);
+	d_mpr_w("%s: cmd:%d\n", __func__, msg->hd.cmd_id);
 	switch (msg->hd.cmd_id) {
 	case	MMRM_VM_RESPONSE_REGISTER:
 		node->m_resp->msg.data.reg.client_id = msg->data.reg.client_id;
-		d_mpr_h("%s: client:%d\n", __func__, msg->data.reg.client_id);
+		d_mpr_e("%s: client_id:%u\n", __func__, msg->data.reg.client_id);
 		break;
 	case	MMRM_VM_RESPONSE_SETVALUE:
 		node->m_resp->msg.data.setval.val = msg->data.setval.val;
@@ -85,22 +88,31 @@ int mmrm_vm_fe_request_send(struct mmrm_vm_driver_data *mmrm_vm,
 		return -E2BIG;
 	}
 
-	d_mpr_h("%s: handle=%p\n", __func__, pmsg_info->msgq_handle);
 	rc = gh_msgq_send(pmsg_info->msgq_handle, msg_pkt,
-			msg_size + sizeof(msg_pkt->hdr), GH_MSGQ_TX_PUSH);
-
-	d_mpr_h("%s: handle=%p result:%d\n", __func__, pmsg_info->msgq_handle, rc);
+		msg_size + sizeof(msg_pkt->hdr), GH_MSGQ_TX_PUSH);
 
 	return rc;
+}
+
+int mmrm_vm_fe_count_clk_clients_frm_dt(struct platform_device *pdev)
+{
+	u32 size_clk_src = 0, num_clk_src = 0;
+
+	of_find_property(pdev->dev.of_node, "mmrm-client-info", &size_clk_src);
+	num_clk_src = size_clk_src / sizeof(struct mmrm_vm_fe_clk_client_desc);
+	d_mpr_h("%s: found %d clk_srcs size %d\n",
+		__func__, num_clk_src, size_clk_src);
+
+	return num_clk_src;
 }
 
 int mmrm_vm_fe_load_clk_rsrc(struct mmrm_vm_driver_data *mmrm_vm)
 {
 	int rc = 0, num_clk_src = 0;
-	int c = 0, size_clk_src = 0, entry_offset = 5;
+	int c = 0, size_clk_src = 0, entry_offset = 3;
 
 	struct platform_device *pdev;
-	struct mmrm_vm_fe_clk_src_info *pclk_src;
+	struct mmrm_vm_fe_clk_client_desc *pclk_src;
 	struct mmrm_vm_fe_priv *fe_data = mmrm_vm->vm_pvt_data;
 
 	pdev = container_of(fe_data->dev, struct platform_device, dev);
@@ -123,10 +135,10 @@ int mmrm_vm_fe_load_clk_rsrc(struct mmrm_vm_driver_data *mmrm_vm)
 		rc = -ENOMEM;
 		goto err_load_clk_src_tbl;
 	}
-	num_clk_src = size_clk_src / sizeof(struct mmrm_clk_client_desc);
+	num_clk_src = size_clk_src / sizeof(struct mmrm_vm_fe_clk_client_desc);
 	fe_data->clk_src_set.count = num_clk_src;
 
-	d_mpr_h("%s: found %d clk_srcs size %d\n",
+	d_mpr_w("%s: found %d clk_srcs size %d\n",
 			__func__, num_clk_src, size_clk_src);
 
 	for (c = 0; c < num_clk_src; c++) {
@@ -134,10 +146,13 @@ int mmrm_vm_fe_load_clk_rsrc(struct mmrm_vm_driver_data *mmrm_vm)
 
 		of_property_read_u32_index(pdev->dev.of_node,
 			"mmrm-client-info", (c*entry_offset),
-			&pclk_src->client_desc.client_domain);
+			&pclk_src->client_domain);
 		of_property_read_u32_index(pdev->dev.of_node,
 			"mmrm-client-info", (c*entry_offset+1),
-			&pclk_src->client_desc.client_id);
+			&pclk_src->client_id);
+		of_property_read_u32_index(pdev->dev.of_node,
+			"mmrm-client-info", (c*entry_offset+2),
+			&pclk_src->num_hw_blocks);
 	}
 
 	return 0;
@@ -146,24 +161,68 @@ err_load_clk_src_tbl:
 	return rc;
 }
 
-void init_lookup_table(struct mmrm_vm_driver_data *mmrm_vm)
+struct mmrm_vm_fe_clk_client_desc *mmrm_vm_fe_clk_src_get(struct mmrm_client_desc *desc)
 {
-	int  i;
+	struct mmrm_vm_fe_priv *fe_data = drv_vm_fe->vm_pvt_data;
+	int num_clk_src = fe_data->clk_src_set.count;
+	struct mmrm_vm_fe_clk_client_desc *pclk_src;
+	int i;
+
+	d_mpr_l("%s: num clk src=%d domain:%d id:%d\n", __func__, num_clk_src,
+		desc->client_info.desc.client_domain, desc->client_info.desc.client_id);
+
+	pclk_src = fe_data->clk_src_set.clk_src_tbl;
+	for (i = 0; i < num_clk_src; i++, pclk_src++) {
+		if (pclk_src->client_domain == desc->client_info.desc.client_domain ||
+			pclk_src->client_id == desc->client_info.desc.client_id) {
+			return pclk_src;
+		}
+	}
+	return NULL;
+}
+
+int mmrm_vm_fe_init_lookup_table(struct mmrm_vm_driver_data *mmrm_vm)
+{
+	int  i, rc = -1;
 	struct platform_device *pdev;
 	struct mmrm_vm_fe_priv *fe_data = mmrm_vm->vm_pvt_data;
 
 	pdev = container_of(fe_data->dev, struct platform_device, dev);
-	mmrm_vm_fe_load_clk_rsrc(mmrm_vm);
 
 	fe_data->client_tbl = devm_kzalloc(&pdev->dev,
-			fe_data->clk_src_set.count * sizeof(struct mmrm_client), GFP_KERNEL);
+		fe_data->clk_src_set.count * sizeof(struct mmrm_client), GFP_KERNEL);
 	if (!fe_data->client_tbl)
-		return;
+		return rc;
 
 	for (i = 0; i < fe_data->clk_src_set.count; i++) {
 		fe_data->client_tbl[i].client_type = 0;
-		fe_data->client_tbl[i].client_uid = 0;
+		fe_data->client_tbl[i].client_uid = U32_MAX;
 	}
+	return 0;
+}
+
+int mmrm_vm_fe_clk_print_info(
+	struct mmrm_vm_fe_clk_src_set *clk_src_set,
+	char *buf, int max_len)
+{
+	int left_spaces = max_len;
+	int len, c;
+	struct mmrm_vm_fe_clk_client_desc *pclk_src;
+	int num_clk_src = clk_src_set->count;
+
+	len = scnprintf(buf, left_spaces, "Domain  ID  Num\n");
+	left_spaces -= len;
+	buf += len;
+
+	pclk_src = clk_src_set->clk_src_tbl;
+	for (c = 0; c < num_clk_src; c++, pclk_src++) {
+		len = scnprintf(buf, left_spaces, "%d\t%d\t%d\n",
+			pclk_src->client_domain, pclk_src->client_id, pclk_src->num_hw_blocks);
+		left_spaces -= len;
+		buf += len;
+	}
+
+	return max_len - left_spaces;
 }
 
 struct mmrm_client *mmrm_vm_fe_get_client(u32 client_id)
@@ -172,14 +231,17 @@ struct mmrm_client *mmrm_vm_fe_get_client(u32 client_id)
 	struct mmrm_client *ptr;
 	struct mmrm_vm_fe_priv *fe_data = drv_vm_fe->vm_pvt_data;
 
+	if (client_id == U32_MAX)
+		return NULL;
+
 	for (i = 0, ptr = fe_data->client_tbl; i < fe_data->clk_src_set.count; i++, ptr++) {
-		if (ptr->client_uid == client_id+1)
+		if (ptr->client_uid == client_id)
 			return ptr;
 	}
 
 	for (i = 0, ptr = fe_data->client_tbl; i < fe_data->clk_src_set.count; i++, ptr++) {
-		if (ptr->client_uid == 0) {
-			ptr->client_uid = client_id + 1;
+		if (ptr->client_uid == U32_MAX) {
+			ptr->client_uid = client_id;
 			return ptr;
 		}
 	}
