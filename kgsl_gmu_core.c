ff7a53f6c10e459750a4df3f5f36a238800bf4bf
Pankaj Gupta <quic_gpankaj@quicinc.com>
2022-04-25 16:13:40 +0530

    msm: kgsl: Do not capture GMU snapshot until snapshot->recovered is set
    When we are in a middle of a snapshot capture, snapshot->recovered will
    be set to false. Skip snapshot capture for GMU fault when we already in
    a middle of snapshot collection for some other fault.
    
    Change-Id: I92cfcabaf76977728bc27ff525984f63a43a439e
    Signed-off-by: Pankaj Gupta <quic_gpankaj@quicinc.com>
    Signed-off-by: Lynus Vaz <quic_lvaz@quicinc.com>

--- kgsl_gmu_core.c
+++ kgsl_gmu_core.c
@@ -165,7 +165,8 @@ int gmu_core_dev_wait_for_active_transition(struct kgsl_device *device)
 void gmu_core_fault_snapshot(struct kgsl_device *device)
 {
 	device->gmu_fault = true;
-	kgsl_device_snapshot(device, NULL, NULL, true);
+	if (device->snapshot->recovered)
+		kgsl_device_snapshot(device, NULL, NULL, true);
 }
 
 int gmu_core_timed_poll_check(struct kgsl_device *device,
