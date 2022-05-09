03201ede0d00eed5f6977df11dc8385ac6e9daab
Mohammed Mirza Mandayappurath Manzoor <quic_mmandaya@quicinc.com>
2022-05-09 10:33:33 -0700

    msm: kgsl: Enable Battery Current Limitter on gen_7_2_x
    BCL prevents under voltage and provides overcurrent protection.
    
    Change-Id: I45cc2c6cf708c83637ed7ca84973796f7590261b
    Signed-off-by: Mohammed Mirza Mandayappurath Manzoor <quic_mmandaya@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -2035,7 +2035,8 @@ static const struct adreno_gen7_core adreno_gpu_core_gen7_2_0 = {
 				UINT_MAX, UINT_MAX, UINT_MAX, ANY_ID),
 		.compatible = "qcom,adreno-gpu-gen7-2-0",
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT | ADRENO_IFPC |
-				ADRENO_CONTENT_PROTECTION | ADRENO_ACD | ADRENO_LPAC,
+				ADRENO_CONTENT_PROTECTION | ADRENO_ACD |
+				ADRENO_LPAC | ADRENO_BCL,
 		.gpudev = &adreno_gen7_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen7_2_0_perfcounters,
 		.gmem_base = 0x1000000,
@@ -2066,7 +2067,8 @@ static const struct adreno_gen7_core adreno_gpu_core_gen7_2_1 = {
 				UINT_MAX, UINT_MAX, UINT_MAX, ANY_ID),
 		.compatible = "qcom,adreno-gpu-gen7-2-1",
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT | ADRENO_IFPC |
-				ADRENO_CONTENT_PROTECTION | ADRENO_LPAC,
+				ADRENO_CONTENT_PROTECTION | ADRENO_LPAC |
+				ADRENO_BCL,
 		.gpudev = &adreno_gen7_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen7_2_0_perfcounters,
 		.gmem_base = 0x1000000,
