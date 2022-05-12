ad0c51b3d4da98fdd912015c790c749f94b29ef8
Kamal Agrawal <quic_kamaagra@quicinc.com>
2022-05-12 23:27:59 +0530

    msm: kgsl: Process CX gdsc event only if KGSL is waiting for it
    It is possible that other clients of CX gdsc are turning the regulator
    on and off even when GPU is off. There is no need to process such
    notifier events. Hence, acknowledge the CX gdsc notifier event only if
    KGSL is waiting for it.
    
    Change-Id: I4f7e4cec9c0a25fd5ec98c56cbc0f5858ba5befd
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>

--- adreno_a6xx_gmu.c
+++ adreno_a6xx_gmu.c
@@ -497,6 +497,7 @@ int a6xx_gmu_enable_gdsc(struct adreno_device *adreno_dev)
 		dev_err(&gmu->pdev->dev,
 			"Failed to enable GMU CX gdsc, error %d\n", ret);
 
+	clear_bit(GMU_PRIV_CX_GDSC_WAIT, &gmu->flags);
 	return ret;
 }
 
@@ -509,6 +510,7 @@ void a6xx_gmu_disable_gdsc(struct adreno_device *adreno_dev)
 	if (ADRENO_QUIRK(adreno_dev, ADRENO_QUIRK_CX_GDSC))
 		regulator_set_mode(gmu->cx_gdsc, REGULATOR_MODE_IDLE);
 
+	set_bit(GMU_PRIV_CX_GDSC_WAIT, &gmu->flags);
 	regulator_disable(gmu->cx_gdsc);
 
 	if (ADRENO_QUIRK(adreno_dev, ADRENO_QUIRK_CX_GDSC))
@@ -2680,7 +2682,8 @@ static int gmu_cx_gdsc_event(struct notifier_block *nb,
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
 	u32 val, offset;
 
-	if (!(event & REGULATOR_EVENT_DISABLE))
+	if (!(event & REGULATOR_EVENT_DISABLE) ||
+		!test_bit(GMU_PRIV_CX_GDSC_WAIT, &gmu->flags))
 		return 0;
 
 	offset = (adreno_is_a662(ADRENO_DEVICE(device)) ||
@@ -2691,6 +2694,7 @@ static int gmu_cx_gdsc_event(struct notifier_block *nb,
 		!(val & BIT(31)), 100, 100 * 1000))
 		dev_err(device->dev, "GPU CX wait timeout.\n");
 
+	clear_bit(GMU_PRIV_CX_GDSC_WAIT, &gmu->flags);
 	complete_all(&gmu->gdsc_gate);
 
 	return 0;
