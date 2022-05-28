8ccaf333a3c5b208a3f7045185aa9d5f5f74538e
Chaitanya Pratapa <quic_cpratapa@quicinc.com>
2022-05-27 17:26:09 -0700

    dataipa: Add KERNEL_DLKM check for module compilation
    Compile vendor DLKMs only if the system wide flag is true or
    if the project specific override flag is set.
    
    Change-Id: I744fce818c59d5072c0e085a41c9225fc317e711
    Signed-off-by: Chaitanya Pratapa <quic_cpratapa@quicinc.com>

--- dataipa_dlkm_vendor_board.mk
+++ dataipa_dlkm_vendor_board.mk
@@ -1,4 +1,14 @@
 #Build ipa
+TARGET_DATAIPA_DLKM_ENABLE := false
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+	ifeq ($(TARGET_KERNEL_DLKM_DATAIPA_OVERRIDE), true)
+		TARGET_DATAIPA_DLKM_ENABLE := true
+	endif
+else
+	TARGET_DATAIPA_DLKM_ENABLE := true
+endif
+
+ifeq ($(TARGET_DATAIPA_DLKM_ENABLE), true)
 DATA_DLKM_BOARD_PLATFORMS_LIST := taro kalama
 ifneq ($(TARGET_BOARD_AUTO),true)
 ifeq ($(call is-board-platform-in-list,$(DATA_DLKM_BOARD_PLATFORMS_LIST)),true)
@@ -9,6 +19,7 @@ BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/rndisipam.ko
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/ipa_clientsm.ko
 ifeq ($(CONFIG_LOCALVERSION), "-gki-consolidate")
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/ipatestm.ko
+endif
 endif
 endif
 endif
