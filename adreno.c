2dd7dc81e49308437764f3b9865d699edf91d965
Lynus Vaz <quic_lvaz@quicinc.com>
2022-06-06 09:09:19 -0700

    msm: kgsl: Select the pwrlevel table based on the socinfo
    Use the SoC pcode and featurecode values as obtained from the socinfo
    driver to determine the pwrlevel table that should be used on the
    device.
    
    Change-Id: I3085516e7d3bd0f8ffd31fe479658f02e298fe92
    Signed-off-by: Arya Anand <quic_aryaanan@quicinc.com>

--- adreno.c
+++ adreno.c
@@ -19,6 +19,7 @@
 #include <linux/soc/qcom/llcc-qcom.h>
 #include <linux/trace.h>
 #include <soc/qcom/dcvs.h>
+#include <soc/qcom/socinfo.h>
 
 #include "adreno.h"
 #include "adreno_a3xx.h"
@@ -747,18 +748,42 @@ static int adreno_of_get_pwrlevels(struct adreno_device *adreno_dev,
 {
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
 	struct device_node *node, *child;
-	unsigned int bin = 0;
+	int feature_code, pcode;
+	u32 soc_code;
 
 	node = of_find_node_by_name(parent, "qcom,gpu-pwrlevel-bins");
 	if (node == NULL)
 		return adreno_of_get_legacy_pwrlevels(adreno_dev, parent);
 
+	feature_code = max_t(int, socinfo_get_feature_code(), SOCINFO_FC_UNKNOWN);
+	pcode = (feature_code >= SOCINFO_FC_Y0 && feature_code < SOCINFO_FC_INT_RESERVE) ?
+		max_t(int, socinfo_get_pcode(), SOCINFO_PCODE_UNKNOWN) : SOCINFO_PCODE_UNKNOWN;
+
+	soc_code = FIELD_PREP(GENMASK(31, 16), pcode) | FIELD_PREP(GENMASK(15, 0), feature_code);
+
 	for_each_child_of_node(node, child) {
+		bool match = false;
+		int tbl_size;
+		u32 bin = 0;
 
-		if (of_property_read_u32(child, "qcom,speed-bin", &bin))
-			continue;
+		if (!of_property_read_u32(child, "qcom,speed-bin", &bin))
+			match = bin == device->speed_bin;
+		else if (of_get_property(child, "qcom,sku-codes", &tbl_size)) {
+			int num_codes = tbl_size / sizeof(u32);
+			int i;
+			u32 sku_code;
 
-		if (bin == device->speed_bin) {
+			for (i = 0; i < num_codes; i++) {
+				if (!of_property_read_u32_index(child, "qcom,sku-codes",
+								i, &sku_code) &&
+					(sku_code == 0 || soc_code == sku_code)) {
+					match = true;
+					break;
+				}
+			}
+		}
+
+		if (match) {
 			int ret;
 
 			ret = adreno_of_parse_pwrlevels(adreno_dev, child);
@@ -782,8 +807,8 @@ static int adreno_of_get_pwrlevels(struct adreno_device *adreno_dev,
 	}
 
 	dev_err(&device->pdev->dev,
-		"GPU speed_bin:%d mismatch for bin:%d\n",
-		device->speed_bin, bin);
+		"No match for speed_bin:%d or soc_code:0x%x\n",
+		device->speed_bin, soc_code);
 	return -ENODEV;
 }
 
