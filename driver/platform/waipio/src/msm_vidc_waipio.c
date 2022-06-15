6bf3bcb9cca4e84baef4534a3ac3cdbaa9770651
Renjiang Han <quic_renjiang@quicinc.com>
2022-06-15 10:21:54 +0800

    video: driver: vp9 1080p decoder needs to reach 180fps
    cts requires vp9 1080p decoder to achieve 180fps on AndroidT.
    
    Change-Id: I68f53a661cb9644dee085652e19962e3bac132c4
    Signed-off-by: Renjiang Han <quic_renjiang@quicinc.com>

--- driver/platform/waipio/src/msm_vidc_waipio.c
+++ driver/platform/waipio/src/msm_vidc_waipio.c
@@ -39,7 +39,7 @@
 #define VP9     MSM_VIDC_VP9
 #define HEIC    MSM_VIDC_HEIC
 #define CODECS_ALL     (H264 | HEVC | VP9 | HEIC)
-#define MAXIMUM_OVERRIDE_VP9_FPS 120
+#define MAXIMUM_OVERRIDE_VP9_FPS 180
 
 static struct msm_platform_core_capability core_data_waipio[] = {
 	/* {type, value} */
