041ab70c3f1ae99f6fee90503fa7a5c83db25479
quic_aryaanan <quic_aryaanan@quicinc.com>
2022-07-06 13:22:01 -0700

    Revert "msm: kgsl: Select the pwrlevel table based on the socinfo"
    This reverts commit 1e3c43fc75599c457c1e40f3665c955451d9a3a1.
    
    Change-Id: I1a16e39b640071ad8406013f9313f2a7e67105f8
    Signed-off-by: Arya Anand <quic_aryaanan@quicinc.com>

--- adreno.c
+++ adreno.c
@@ -19,7 +19,6 @@
 #include <linux/soc/qcom/llcc-qcom.h>
 #include <linux/trace.h>
 #include <soc/qcom/dcvs.h>
-#include <soc/qcom/socinfo.h>
 
 #include "adreno.h"
 #include "adreno_a3xx.h"
@@ -747,43 +746,18 @@ static int adreno_of_get_pwrlevels(struct adreno_device *adreno_dev,
 {
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
 	struct device_node *node, *child;
-	int feature_code = socinfo_get_feature_code();
-	int pcode;
-	u32 soc_code;
-
-	feature_code = max_t(int, feature_code, 0);
-	pcode = (feature_code >= SOCINFO_FC_Y0 && feature_code < SOCINFO_FC_INT_RESERVE) ?
-		max_t(int, socinfo_get_pcode(), 0) : 0;
-
-	soc_code = (pcode << 8) | feature_code;
+	unsigned int bin = 0;
 
 	node = of_find_node_by_name(parent, "qcom,gpu-pwrlevel-bins");
 	if (node == NULL)
 		return adreno_of_get_legacy_pwrlevels(adreno_dev, parent);
 
 	for_each_child_of_node(node, child) {
-		unsigned int bin = 0;
-		int tbl_size;
-		bool match = false;
 
-		if (!of_property_read_u32(child, "qcom,speed-bin", &bin))
-			match = (bin == device->speed_bin);
-		else if (of_get_property(child, "qcom,sku-codes", &tbl_size)) {
-			int num_codes = tbl_size / sizeof(u32);
-			int i;
-			u32 sku_code;
+		if (of_property_read_u32(child, "qcom,speed-bin", &bin))
+			continue;
 
-			for (i = 0; i < num_codes; i++) {
-				if (!of_property_read_u32_index(child, "qcom,sku-codes",
-								i, &sku_code) &&
-					soc_code == sku_code) {
-					match = true;
-					break;
-				}
-			}
-		}
-
-		if (match) {
+		if (bin == device->speed_bin) {
 			int ret;
 
 			ret = adreno_of_parse_pwrlevels(adreno_dev, child);
@@ -807,8 +781,8 @@ static int adreno_of_get_pwrlevels(struct adreno_device *adreno_dev,
 	}
 
 	dev_err(&device->pdev->dev,
-		"No match for speed_bin: %d soc_code: %d\n",
-		device->speed_bin, soc_code);
+		"GPU speed_bin:%d mismatch for bin:%d\n",
+		device->speed_bin, bin);
 	return -ENODEV;
 }
 
