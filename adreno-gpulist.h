db7c60d3c35cda762af2a2d52869a9d06ad402b6
Archana Sriram <quic_c_apsrir@quicinc.com>
2022-07-18 16:45:27 +0530

    msm: kgsl: Add snapshot_size for A610 GPU
    Add snapshot_size for A610 GPU in Adreno header.
    
    Change-Id: I6a1655166206d5abba979e186e0c9973174cc7b6
    Signed-off-by: Archana Sriram <quic_c_apsrir@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -1504,6 +1504,7 @@ static const struct adreno_a6xx_core adreno_gpu_core_a610 = {
 		.uche_gmem_alignment = SZ_1M,
 		.gmem_size = (SZ_128K + SZ_4K),
 		.bus_width = 32,
+		.snapshot_size = SZ_1M,
 	},
 	.prim_fifo_threshold = 0x00080000,
 	.sqefw_name = "a630_sqe.fw",
