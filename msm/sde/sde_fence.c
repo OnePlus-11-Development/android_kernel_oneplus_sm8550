b4a071ae7f2660175e531fe21b4212a343b99ce8
Christina Oliveira <quic_coliveir@quicinc.com>
2022-07-28 13:18:23 -0700

    disp: msm: sde: disable hw-fencing for commit before vm transition
    This change disables hw-fencing for the last commit before
    vm transition. This avoids configuration issues if hw-fencing is
    disabled in the incoming VM.
    
    Change-Id: I573b7d1665f8cef442168bd0ab83a4b2b6cebbb6
    Signed-off-by: Christina Oliveira <quic_coliveir@quicinc.com>

--- msm/sde/sde_fence.c
+++ msm/sde/sde_fence.c
@@ -469,7 +469,7 @@ static int _reset_hw_fence_timeline(struct sde_hw_ctl *hw_ctl, u32 flags)
 }
 
 int sde_fence_update_input_hw_fence_signal(struct sde_hw_ctl *hw_ctl, u32 debugfs_hw_fence,
-		struct sde_hw_mdp *hw_mdp)
+		struct sde_hw_mdp *hw_mdp, bool disable)
 {
 	struct sde_hw_fence_data *data;
 	u32 ipcc_signal_id;
@@ -485,6 +485,11 @@ int sde_fence_update_input_hw_fence_signal(struct sde_hw_ctl *hw_ctl, u32 debugf
 
 	ctl_id = hw_ctl->idx - CTL_0;
 	data = &hw_ctl->hwfence_data;
+
+	if (disable) {
+		hw_ctl->ops.hw_fence_ctrl(hw_ctl, false, false, 0);
+		return -EPERM;
+	}
 
 	if ((debugfs_hw_fence & SDE_INPUT_HW_FENCE_TIMESTAMP)
 			&& hw_mdp->ops.hw_fence_input_timestamp_ctrl)
