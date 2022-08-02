d7e4bd13f3e73ff28bf04780d4c08b14989272af
Michael Adisumarta <quic_madisuma@quicinc.com>
2022-08-02 08:35:24 -0700

    dataipa: Includes changes for IPA Peripheral stats for Q6
    Definition and support for MDM/MSM peripheral stats structure,
    shared to Q6 on the first 2KB of already existing FILTER_TABLE smem.
    
    Change-Id: I1742beeb131beb97340d494135880b1906110a2e
    Signed-off-by: Michael Adisumarta <quic_madisuma@quicinc.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -11217,6 +11217,19 @@ static int ipa_smmu_ap_cb_probe(struct device *dev)
 		ipa3_iommu_map(cb->iommu_domain,
 				iova_p, pa_p, size_p,
 				IOMMU_READ | IOMMU_WRITE);
+
+		ipa3_ctx->per_stats_smem_pa = iova;
+		ipa3_ctx->per_stats_smem_va = smem_addr;
+
+		/**
+		 * Force type casting to perpheral stats structure.
+		 * First 2kB of the FILTER_TABLE SMEM is allocated for
+		 * Peripheral stats design. If there is a need to
+		 * use rest of FILTER_TABLE_SMEM it should be used from
+		 * smem_addr + 2KB offset
+		 */
+		ret = ipa3_peripheral_stats_init((union ipa_peripheral_stats *) smem_addr);
+		if(ret)	IPAERR("IPA Peripheral stats init failure = %d ", ret);
 	}
 
 	smmu_info.present[IPA_SMMU_CB_AP] = true;
