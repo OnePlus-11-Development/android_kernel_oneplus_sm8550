65d0e85a94347a34d336cbc39c5f06ffe54f8808
Sachu George <quic_sachgeor@quicinc.com>
2022-08-12 15:38:27 +0530

    video-driver: Disabling VSP subcache for Halliday
    VSP subcache support is not available in 5.10 kernel.
    Hence needs to be disabled for Halliday.
    
    Change-Id: I8e0905d537d8df3c72063b2e8dc5011d8e0ff785
    Signed-off-by: Sachu George <quic_sachgeor@quicinc.com>

--- driver/vidc/src/venus_hfi.c
+++ driver/vidc/src/venus_hfi.c
@@ -1742,7 +1742,9 @@ static int __init_subcaches(struct msm_vidc_core *core)
 		} else if (!strcmp("vidscfw", sinfo->name)) {
 			sinfo->subcache = llcc_slice_getd(LLCC_VIDFW);
 		} else if (!strcmp("vidvsp", sinfo->name)) {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,15,0))
 			sinfo->subcache = llcc_slice_getd(LLCC_VIDVSP);
+#endif
 		} else {
 			d_vpr_e("Invalid subcache name %s\n",
 					sinfo->name);
