489d363e9076bc1b17f60c7eee639de2579f27ef
Hareesh Gundu <quic_hareeshg@quicinc.com>
2022-10-07 20:51:54 -0700

    msm: kgsl: Update gpu work period logging
    Parallel GPU work can be submitted from several different
    processes with the same application UID. Hence update gpu
    work period logging based on application uid instead of PID.
    
    Change-Id: Ifb18da3a8c251a8dd64b74e2088517d4e6150131
    Signed-off-by: Hareesh Gundu <quic_hareeshg@quicinc.com>

--- adreno_dispatch.c
+++ adreno_dispatch.c
@@ -250,7 +250,7 @@ static void _retire_timestamp(struct kgsl_drawobj *drawobj)
 
 	if (drawobj->flags & KGSL_DRAWOBJ_END_OF_FRAME) {
 		atomic64_inc(&context->proc_priv->frame_count);
-		atomic_inc(&context->proc_priv->period.frames);
+		atomic_inc(&context->proc_priv->period->frames);
 	}
 
 	/*
@@ -2219,6 +2219,11 @@ static void retire_cmdobj(struct adreno_device *adreno_dev,
 	info.eop = end;
 	info.active = active;
 
+	/* protected GPU work must not be reported */
+	if  (!(context->flags & KGSL_CONTEXT_SECURE))
+		kgsl_work_period_update(KGSL_DEVICE(adreno_dev),
+					     context->proc_priv->period, active);
+
 	msm_perf_events_update(MSM_PERF_GFX, MSM_PERF_RETIRED,
 			       pid_nr(context->proc_priv->pid),
 			       context->id, drawobj->timestamp,
@@ -2226,7 +2231,7 @@ static void retire_cmdobj(struct adreno_device *adreno_dev,
 
 	if (drawobj->flags & KGSL_DRAWOBJ_END_OF_FRAME) {
 		atomic64_inc(&context->proc_priv->frame_count);
-		atomic_inc(&context->proc_priv->period.frames);
+		atomic_inc(&context->proc_priv->period->frames);
 	}
 
 	/*
